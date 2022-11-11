7a7165c6f87cfdcd136ccbef87208e91f7bda5a7
rikako <jc180215b@gmail.com>
2022-11-11 15:13:42 +0900

    modify:error_message

---
+++ workspace/contacts/src/main/java/com/example/contacts/controller/PersonController.java
@@ -0,0 +1,29 @@
+package com.example.contacts.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.contacts.models.Person;
+
+@Controller
+public class PersonController {
+
+  @GetMapping("/")
+  public String index(@ModelAttribute Person person) {
+    return "person/index";
+  }
+
+  @PostMapping("/create")
+  public String create(@Validated @ModelAttribute Person person, BindingResult result) {
+
+    //バリデーションエラーがある場合はindex.htmlを表示
+    if (result.hasErrors()){
+      return "person/index";
+    }
+    return "person/create";
+  }
+}
