6eab3a2e7274ed7cd006968b4cb7da53ead106ed
rikako <jc180215b@gmail.com>
2022-11-28 14:52:04 +0900

    modify:コメント

--- Downloads/techpit-javascript-seat-startdocs-master/assets/js/script.js
+++ Downloads/techpit-javascript-seat-startdocs-master/assets/js/script.js
@@ -1,3 +1,9 @@
+/**
+  * 数字の席番号ブロックを表示するための元データになる配列を作成
+  * 関数内で、欠席者を取り除く処理も行う
+  * @param  {Number} studentNumber 生徒の人数を表示 
+  * @return {Array} 席番号ブロックに表示するための元データとなる配列（欠席者取り除き済）
+  */
 
 const setTargetStudents = (studentNumber)=>{
   let studentNumberList = [];
@@ -16,6 +22,12 @@ const setTargetStudents = (studentNumber)=>{
   return studentNumberList;
 }
 
+/**
+  * 席番号ブロックの元データが入った配列の中身をシャッフルさせる
+  * @type {Array} studentNumberList ・・・画面に表示対象となる生徒の学籍番号が格納されたもの
+  * @return {Array} studentNumberListを基に中身の数字がシャッフルされた配列
+  */
+
 const shuffleArray = (studentNumberList)=>{
   for(let i = studentNumberList.length; i > 0; i--){
     const randomNum = Math.floor(Math.random() * i);
@@ -27,6 +39,11 @@ const shuffleArray = (studentNumberList)=>{
   return studentNumberList;
 }
 
+/**
+  * 数字がシャッフルされた席番号ブロックの元データとなる配列を画面上に表示させる
+  * @type {Array} shuffleStudent ・・・生徒の学籍番号がシャッフルされた状態で格納されたもの
+*/
+
 const showSeatBoxes = (shuffleStudent)=>{
   let insertHTML = '';
   shuffleStudent.forEach(function(num){
@@ -34,6 +51,11 @@ const showSeatBoxes = (shuffleStudent)=>{
   })
   document.querySelector('#seat').innerHTML = insertHTML;
 }
+
+/**
+  * @type {Function} timer ・・・・・ setInterval関数が格納されている
+  * @type {Object} audioElement ・・・音声データとなるもの
+  */
 
 const soundPlay = (timer)=>{
   const audioElement = new Audio();
@@ -44,6 +66,13 @@ const soundPlay = (timer)=>{
     clearInterval(timer);
   })
 }
+
+/**
+  * モーダル内のスタートボタンが押された際の動作を記述
+  * @type {Number} studentNumber・・・入力欄に入力された人数
+  * @type {Number} studentUpperlimit・・・人数の入力欄の上限値
+  * @type {Boolean} studentNumberIsEmpty・・・人数の入力欄に数値が入力されたかどうか
+  */
 
 document.querySelector('#btn-start').addEventListener('click', ()=>{
   const studentNumber = document.querySelector("#studentNumber").value;
