63bde9d4ab6c18f05d39b25d79e9d1b04252865c
rikako <jc180215b@gmail.com>
2023-03-20 13:25:50 +0900

    データベースの操作

--- Desktop/SpringBasicSource/WebBeginnerStarter/src/main/java/com/example/demo/app/SampleController.java
+++ Desktop/SpringBasicSource/WebBeginnerStarter/src/main/java/com/example/demo/app/SampleController.java
@@ -1,27 +1,37 @@
 package com.example.demo.app;
 
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/test")
+@RequestMapping("/")
 
 public class SampleController {
-	
-// 	private final JdbcTemplate jdbcTemplate;
 
-// 	//Add an annotation here 
-// 	public SampleController(JdbcTemplate jdbcTemplate) {
-// 		this.jdbcTemplate = jdbcTemplate;
-// 	}
-	
-	@GetMapping("/test")
+	private final JdbcTemplate jdbcTemplate;
+
+	@Autowired
+	public SampleController(JdbcTemplate jdbcTemplate) {
+ 		this.jdbcTemplate = jdbcTemplate;
+ 	}
+
+	@GetMapping("/")
 	public String test(Model model) {
-		model.addAttribute("title", "Inquiry Form");
 
-		return "test";
+		String sql = "SELECT id, name, email "
+		       + "FROM inquiry WHERE id = 1";
+		Map<String, Object> map = jdbcTemplate.queryForMap(sql);
+		model.addAttribute("title", "Inquiry Form");
+		model.addAttribute("name", map.get("name"));
+		model.addAttribute("email", map.get("email"));
+
+		return "index";
 	}
 
 }
