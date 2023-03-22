3263ee0bf38364ae75a957569ab437a3c8940820
rikako <jc180215b@gmail.com>
2023-03-22 20:22:50 +0900

    アンケートフォームのデータベース部分

--- Desktop/SpringBasicSource/WebBeginnerStarter/src/main/java/com/example/demo/app/survey/SurveyController.java
+++ Desktop/SpringBasicSource/WebBeginnerStarter/src/main/java/com/example/demo/app/survey/SurveyController.java
@@ -1,5 +1,9 @@
 package com.example.demo.app.survey;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import com.example.demo.entity.Survey;
 import com.example.demo.service.SurveyService;
 
 @Controller
@@ -18,6 +23,7 @@ public class SurveyController {
 
 	private final SurveyService surveyService;
 
+	@Autowired
 	public SurveyController(SurveyService surveyService){
 		this.surveyService = surveyService;
 	}
@@ -25,7 +31,9 @@ public class SurveyController {
 	@GetMapping
 	public String index(Model model) {
 
-		//hands-on
+		List<Survey> list = surveyService.getAll();
+		model.addAttribute("surveyList", list);
+		model.addAttribute("title", "Survey Index");
 
 		return "survey/index";
 	}
@@ -73,6 +81,15 @@ public class SurveyController {
 			model.addAttribute("title", "Survey Form");
 			return "survey/form";
 		}
+		
+		Survey survey = new Survey();
+		survey.setAge(surveyForm.getAge());
+		survey.setSatisfaction(surveyForm.getSatisfaction());
+		survey.setComment(surveyForm.getComment());
+		survey.setCreated(LocalDateTime.now());
+
+		surveyService.save(survey);
+
 		redirectAttributes.addAttribute("complete", "Resisterd!");
 		return "redirect:/survey/form";
 	}
